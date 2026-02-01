f730ee56088d4a9208873c8b7a2cd085deb01803
Caio Rossi <caio_rossi56@hotmail.com>
2026-02-01 17:20:25 -0300

    feat: add footer structure

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,59 @@
-const Footer = () => {
-  return <div></div>;
-};
+import "../assets/styles/Footer.scss";
 
-export default Footer;
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-columns">
+          <div className="footer-column">
+            <h4>Fingertipe</h4>
+            <ul>
+              <li>Home</li>
+              <li>Examples</li>
+              <li>Pricing</li>
+              <li>Updates</li>
+            </ul>
+          </div>
+
+          <div className="footer-column">
+            <h4>Resources</h4>
+            <ul>
+              <li>Home</li>
+              <li>Examples</li>
+              <li>Pricing</li>
+              <li>Updates</li>
+            </ul>
+          </div>
+
+          <div className="footer-column">
+            <h4>About</h4>
+            <ul>
+              <li>Home</li>
+              <li>Examples</li>
+              <li>Pricing</li>
+              <li>Updates</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="footer-contact">
+          <div className="footer-contact-item">
+            <span></span>
+            <p>7480 Mockingbird Hill undefined</p>
+          </div>
+
+          <div className="footer-contact-item">
+            <span></span>
+            <p>(239) 555-0108</p>
+          </div>
+
+          <div className="footer-social">
+            <span>i</span>
+            <span>i</span>
+            <span>i</span>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
